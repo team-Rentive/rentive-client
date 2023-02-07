d880765236629a0653f0622fdd7c368c3ae4a77e
seongminn <berry9901@gmail.com>
2023-02-07 17:21:40 +0900

    chore(root): 절대 경로 세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,20 @@ module.exports = {
 		'no-console': 'warn',
 		'react/react-in-jsx-scope': 'off',
 		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'import/prefer-default-export': 'off',
+		// airbnb eslint의 import/extensions 조건을 해제하는 옵션
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+				json: 'never',
+				'': 'never',
+			},
+		],
 	},
 	parserOptions: {
 		project: ['./tsconfig.json'], // Specify it only for TypeScript files
